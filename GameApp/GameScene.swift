5fd535b77847eec204972cd5feb346def5790b8d
Enrique J. Pizarro <enriquejavierpizarro@gmail.com>
2020-11-30 11:15:41 -0400

    We created GameScene.swift editions take place on ViewController.swift and GameScene.swift check comments and notebook

---
+++ GameApp/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  GameApp
+//
+//  Created by javier pizarro on 11/30/20.
+//  Copyright © 2020 javierpizarro. All rights reserved.
+//
+
+
+import SpriteKit
+
+class GameScene: SKScene{/*object GameScene a subclass of SKScene class*/
+    
+    override func didMove(to view: SKView) {//Present object in a SKView
+    
+        self.backgroundColor = .red//display red as background color
+        
+    }
+}
--- GameApp/ViewController.swift
+++ GameApp/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let gameScene = GameScene(size: view.bounds.size)/*GameScene() call object size to be same as the view and assigned to*/
+        
+        let skView = view as! SKView/*it cast(change) default view(UIView) to an SKView. For this line to work we needed before hand to define under
+         custom class the class SKView as we already do. Otherwise the app will crash.*/
+        
+        skView.presentScene(gameScene)//present scene on a skView
+
     }
 
 
