06562afb539a5eda49318206e84b1233e0071163
Enrique J. Pizarro <enriquejavierpizarro@gmail.com>
2020-12-06 15:52:27 -0400

    editions take place on StartScene check notebook and comments

--- GameApp/StartScene.swift
+++ GameApp/StartScene.swift
@@ -37,8 +37,25 @@ class StartScene: SKScene{
         catNode.run(SKAction.repeatForever(animateAction))//animation execution
         
         playButton.setScale(0.5)//scaling down size from default size
-        playButton.position = CGPoint(x: self.size.width/2, y: self.size.height/4)/*positioning for start button at half the width of scene size and one fourth of the height of the scene or bottom center**/
-            addChild(playButton)
-    
+        playButton.position = CGPoint(x: self.size.width/2, y:self.size.height/4)/*positioning for start button at half the width of scene size and one fourth of the height of the scene or bottom center**/
+        addChild(playButton)
+        
+        }
+        /*function below provides an array to store touches and the funtionality to describe an interaction between user and app**/
+       override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            
+            let touch = touches.first//fetch the first element on array
+            if let location = touch?.location(in: self){//if a touch register on StartScene
+                
+                if atPoint(location) == playButton{/*if coordinates on location
+                     match that of start button**/
+                    
+                    let gameScene = GameScene(size: self.size)/*create instantiate GameScene with the same size as StartScene**/
+                    let transition = SKTransition.crossFade(withDuration: 1.5)//transition definition
+                    self.view?.presentScene(gameScene, transition: transition)/*execute change the view to gameScene and a transition**/
+                }
+                    
+            }
+        }
     }
-}
+
